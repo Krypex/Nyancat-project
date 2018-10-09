34b2ca1c3f505c449123988ee3c64757dc0a7730
Rasmus <rasmus.helgesson@elev.ga.lbs.se>
2018-10-09 10:36:09 +0200

    Added the in game graphics and also added scripts to the objects

---
+++ Assets/ColoronShip.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoronShip : MonoBehaviour {
+    public SpriteRenderer rend;
+    public Color newcolor;
+    bool Blue;
+    bool Green;
+    bool Randomcolor;
+    
+	// Use this for initialization
+	void Start () {
+        Blue = false;
+        Green = false;
+        Randomcolor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Ändrar färg till Blå
+        if (Input.GetKey(KeyCode.D))
+        {
+            Blue = true;
+
+            if (Blue == true)
+            {
+                newcolor = rend.color = new Color(0, 0, 1);
+
+            }
+
+        }
+        // Ändrar färg till Grön
+        if (Input.GetKey(KeyCode.A))
+        {
+            Green = true;
+
+            if (Green == true)
+            {
+                newcolor = rend.color = new Color(0, 1, 0);
+
+            }
+
+        }
+        // Random color på alla object
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Randomcolor = true;
+
+            if (Randomcolor == true)
+            {
+                newcolor = rend.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+            }
+
+        }
+    }
+}
---
+++ Assets/SpaceShipscript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipscript : MonoBehaviour {
+    [Range(-720, 720)]
+    public float rotationSpeed;
+    public bool halfspeed;
+   public Transform other;
+    public float Timer;
+    private float variabel = 1;
+    public float newPostions;
+    public float randomspeed;
+    public float newPostionss;
+    // Use this for initialization
+    void Start()
+    {
+        halfspeed = false; // Gör halfspeed of 
+        randomspeed = Random.Range(5, 10); // randomiserar hastigheten i början fårn 1 till 10
+
+        transform.position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5));
+    }  
+    // Update is called once per frame
+	void Update ()
+        {
+        transform.Translate(randomspeed * Time.deltaTime, 0, 0, Space.Self); // Objektet åker fram och Gör hastigheten för skepet oberoende av FPS
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime); // Svänger höger och gör det oberonde av FPS
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);  // Svänger Vänster och gör det oberoende av FPS
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+                transform.Translate(randomspeed/ -2 * Time.deltaTime, 0, 0, Space.Self); // När man Trycker ner S så åker den dubelts så långsammt
+        }
+        //if (Input.GetKeyUp(KeyCode.S))
+        {
+           // transform.Translate(randomspeed * 2 * Time.deltaTime, 0, 0, Space.Self);
+        }
+        Timer += Time.deltaTime; // Timer ökar varenda sekund i real tid
+        if (Timer > variabel && Timer < variabel + 0.2)
+        {
+            Debug.Log(string.Format("Timer:"));
+
+            print(Timer);
+            variabel = (variabel + 1);
+            
+        } 
+        Vector3 newPosition = transform.position; // skapar position
+        if (newPosition.x > 8.89f || newPosition.x < -8.89f)
+        {
+            newPosition.x = -newPosition.x; // sätter ny position x
+
+        }
+        if (newPosition.y > 5.0f || newPosition.y < -5.0f)
+        {
+            newPosition.y = -newPosition.y; // sätter ny position y
+
+        }
+        transform.position = newPosition; // sätter skeppets position
+
+    }
+}
